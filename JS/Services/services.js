702a735d6f21e01b346e4b0620e7f2dcf0400886
rakibhassanrh66 <rakibhassan.rh66@gmail.com>
2025-12-20 00:03:46 +0600

    Marges1

--- JS/Services/services.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello braddayd");
-console.warn("Hello braddayd warning replaying ");
-console.error("Hello braddayd eroor responding");
-
-var name = carlMark;
-var year = 1998;
-
-let name = deadbrat;
-const year = 2024;
